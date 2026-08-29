3700bcf17f04246530cfb41c69b865ad6f4f7a53
Kushaan Mahajan <kushaanmahajan1718@gmail.com>
2026-08-30 03:55:31 +0530

    Version : One Process at a Time

--- src/app/processes/ProcessManagement.cpp
+++ src/app/processes/ProcessManagement.cpp
@@ -11,49 +11,10 @@ bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task) {
 }
 
 void ProcessManagement::executeTasks() {
-    while (!taskQueue.empty()) {
-        std::unique_ptr<Task> taskToExecute = std::move(taskQueue.front());
+    while(!taskQueue.empty()){
+        std::unique_ptr<Task> task = std::move(taskQueue.front());
         taskQueue.pop();
-
-        std::cout << "Executing task: " << taskToExecute->toString() << std::endl;
-        executeCryption(taskToExecute->toString());
-
-        std::string taskStr = taskToExecute->toString();
-
-        // Command to execute
-        std::string command = "cryption.exe \"" + taskStr + "\"";
-
-        STARTUPINFOA si{};
-        PROCESS_INFORMATION pi{};
-
-        si.cb = sizeof(si);
-
-        // Create child process
-        BOOL success = CreateProcessA(
-            nullptr,
-            command.data(),
-            nullptr,
-            nullptr,
-            FALSE,
-            0,
-            nullptr,
-            nullptr,
-            &si,
-            &pi
-        );
-
-        if (success) {
-            // std::cout << "Child process created. PID = "
-            //         << pi.dwProcessId << std::endl;
-
-            WaitForSingleObject(pi.hProcess, INFINITE);
-
-            CloseHandle(pi.hProcess);
-            CloseHandle(pi.hThread);
-        }
-        else {
-            std::cerr << "CreateProcess failed. Error = "
-                    << GetLastError() << std::endl;
-        }
+        std::cout<<"Executing task: "<<task->toString()<<std::endl;
+        executeCryption(task->toString());
     }
 }
